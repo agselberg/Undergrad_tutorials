f68232d1f40f863f70417afcc6698390ecaf4cb2
agselberg <tul06229@temple.edu>
2023-11-07 16:37:16 -0500

    add python ex 1

---
+++ python_exercise1.py
@@ -0,0 +1,21 @@
+############################################################################
+### Python tutorial #2: loading in a tree file and parsing data
+############################################################################
+
+
+### In this tutorial you will practice loading in a file and parsing data
+### You will load in a tree file that was used for a codeml (PAML) analysis and convert it so we can run the same data through hyphy
+# open and read in codeml tree file (note: it's a single line)
+
+
+
+
+
+# for this line, replace '#1" with '{FOREGROUND}'
+
+
+
+
+
+
+# write a new file whose name is the original file name + the extension ".lab.nwk"
